677dd960b11246ae29da16e21eeb874deb4782a5
Khalid-AC <khalid.aici.ac@gmail.com>
2022-11-18 18:14:16 +0100

    home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:weather_app/repository/weather_repository.dart';
+import 'package:weather_app/services/weather_api_services.dart';
+import "package:http/http.dart" as http;
 
 class Homepage extends StatefulWidget {
   Homepage({Key? key}) : super(key: key);
@@ -8,6 +13,22 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  @override
+  void initState() {
+    super.initState();
+    _fetchWeather();
+  }
+
+  _fetchCity() {
+    WeatherApiServices(httpClient: http.Client()).getCity("London");
+  }
+
+  _fetchWeather() {
+    WeatherRepository(
+            weatherApiServices: WeatherApiServices(httpClient: http.Client()))
+        .fetchWeather("London");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
