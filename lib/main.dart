f48b5c52e738e0dc766a4c675ec5578a96588557
Khalid-AC <khalid.aici.ac@gmail.com>
2022-11-18 18:13:15 +0100

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:weather_app/pages/home_page.dart';
 
+void main(List<String> args) {
+  runApp(const MyApp());
+}
 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      title: "Wheather app",
+      debugShowCheckedModeBanner: false,
+
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+
+      home: Homepage(),
+    );
+  }
+}
