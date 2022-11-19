3f76da76d5ae9e6c07b81d9d8dcfbc912e263ddf
Khalid-AC <khalid.aici.ac@gmail.com>
2022-11-19 18:38:36 +0100

    search page finished

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -8,14 +8,74 @@ class Searchpage extends StatefulWidget {
 }
 
 class _SearchpageState extends State<Searchpage> {
+  //
+  final _formKey = GlobalKey<FormState>();
+  String? _city;
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+
+  //
+  _submit() {
+    setState(() {
+      autovalidateMode = AutovalidateMode.always;
+    });
+
+    final form = _formKey.currentState;
+
+    if (form != null && form.validate()) {
+      form.save();
+      Navigator.pop(context, _city!.trim());
+    }
+  }
+
+  //
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Weather"),
+      appBar: AppBar(
+        title: Text("Weather"),
+      ),
+      body: Form(
+        key: _formKey,
+        autovalidateMode: autovalidateMode,
+        child: Column(
+          children: [
+            const SizedBox(height: 60.0),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30.0),
+              child: TextFormField(
+                autofocus: true,
+                style: const TextStyle(fontSize: 18.0),
+                decoration: const InputDecoration(
+                  labelText: "City name",
+                  hintText: "more than 2 characters",
+                  prefixIcon: Icon(Icons.search_rounded),
+                  border: OutlineInputBorder(),
+                ),
+                validator: (String? cityName) {
+                  if (cityName == null || cityName.trim().length < 2) {
+                    return "City name must be at least 2 characters long";
+                  }
+                  return null;
+                },
+                onSaved: (String? cityName) {
+                  _city = cityName;
+                },
+              ),
+            ),
+
+            //
+            SizedBox(height: 20.0),
+
+            //
+            ElevatedButton(
+                onPressed: _submit,
+                child: const Text(
+                  "How's weather",
+                  style: TextStyle(fontSize: 20.0),
+                ))
+          ],
         ),
-        body: Center(
-          child: Text("Search"),
-        ));
+      ),
+    );
   }
 }
