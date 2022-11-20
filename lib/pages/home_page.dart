d00a83d3cfcce94f52af367a47609ef526d3392c
Khalid-AC <khalid.aici.ac@gmail.com>
2022-11-20 20:02:13 +0100

    weather info added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,6 +5,7 @@ import 'package:weather_app/providers/weather_provider.dart';
 import "package:http/http.dart" as http;
 
 import 'package:provider/provider.dart';
+import 'package:weather_app/widgets/error_dialog.dart';
 
 class Homepage extends StatefulWidget {
   Homepage({Key? key}) : super(key: key);
@@ -35,11 +36,7 @@ class _HomepageState extends State<Homepage> {
     final WeatherState ws = context.read<WeatherProvider>().state;
 
     if (ws.weatherStatus == WeatherStatus.error) {
-      showDialog(
-          context: context,
-          builder: (context) {
-            return AlertDialog(content: Text(ws.error.errMsg));
-          });
+      errorDialog(context, ws.error.errMsg);
     }
   }
 
@@ -59,6 +56,7 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.white70,
         appBar: AppBar(
           title: const Text("Weather"),
           actions: [
@@ -84,27 +82,117 @@ class _HomepageState extends State<Homepage> {
         body: _showWeather());
   }
 
+  String showTemperature(double? temperature) {
+    if (temperature == null) return "0";
+    return "${temperature.toStringAsFixed(2)} ℃";
+  }
+
+  Widget showIcon(String icon) {
+    return FadeInImage.assetNetwork(
+      placeholder: "assets/images/loading.gif",
+      image: "http://openweathermap.org/img/wn/$icon@2x.png",
+      width: 64,
+      height: 64,
+    );
+  }
+
   Widget _showWeather() {
     final weatherState = context.watch<WeatherProvider>().state;
 
     if (weatherState.weatherStatus == WeatherStatus.initial) {
       return const Center(
-          child: Text("Selec a city", style: TextStyle(fontSize: 20.0)));
+          child: Text("Select a city", style: TextStyle(fontSize: 20.0)));
     }
 
     if (weatherState.weatherStatus == WeatherStatus.loading) {
       return const Center(child: CircularProgressIndicator());
     }
 
-    if(weatherState.weatherStatus == WeatherStatus.error && weatherState.weather.cityName == ""){
+    if (weatherState.weatherStatus == WeatherStatus.error &&
+        weatherState.weather.cityName == "") {
       return const Center(
           child: Text("Select a city", style: TextStyle(fontSize: 20.0)));
     }
 
-    return Center(
-        child: Text(
-      weatherState.weather.cityName,
-      style: const TextStyle(fontSize: 18.0),
-    ));
+    return ListView(
+      //
+      children: [
+        SizedBox(height: MediaQuery.of(context).size.height / 6),
+
+        Text(
+          weatherState.weather.cityName,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            fontSize: 40.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+
+        //
+        const SizedBox(height: 10.0),
+
+        //
+        Text(
+          TimeOfDay.fromDateTime(weatherState.weather.lastUpdated)
+              .format(context),
+          style: const TextStyle(
+            fontSize: 18.0,
+          ),
+          textAlign: TextAlign.center,
+        ),
+
+        //
+        const SizedBox(height: 60.0),
+
+        //
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          //
+          children: [
+            Text(
+              showTemperature(weatherState.weather.mainTemp),
+              style:
+                  const TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+            ),
+
+            //
+            const SizedBox(width: 20.0),
+
+            //
+            Column(
+              children: [
+                Text(
+                  showTemperature(weatherState.weather.mainTemp),
+                  style: const TextStyle(fontSize: 16.0),
+                ),
+                const SizedBox(height: 10.0),
+                Text(
+                  showTemperature(weatherState.weather.mainTemp),
+                  style: const TextStyle(fontSize: 16.0),
+                ),
+              ],
+            ),
+          ],
+        ),
+
+        //
+        const SizedBox(height: 30.0),
+
+        //
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const Spacer(),
+            showIcon(weatherState.weather.icon),
+            const SizedBox(width: 20.0),
+            Text(
+              weatherState.weather.weatherMain,
+              style: const TextStyle(fontSize: 32.0),
+            ),
+            const Spacer(),
+          ],
+        )
+      ],
+    );
   }
 }
