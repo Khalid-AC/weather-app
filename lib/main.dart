cfa80a13c53c2243694835cd59ae301e491504f0
Khalid-AC <khalid.aici.ac@gmail.com>
2022-11-20 20:55:09 +0100

    injetcing theme provder and temp setting provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:weather_app/pages/home_page.dart';
+import 'package:weather_app/providers/temp_settings_provider.dart';
+import 'package:weather_app/providers/theme_provider.dart';
 import 'package:weather_app/providers/weather_provider.dart';
 import 'package:weather_app/repository/weather_repository.dart';
 import 'package:weather_app/services/weather_api_services.dart';
@@ -22,14 +24,27 @@ class MyApp extends StatelessWidget {
             create: (context) => WeatherProvider(
                 weatherRepository: WeatherRepository(
                     weatherApiServices:
-                        WeatherApiServices(httpClient: http.Client()))))
+                        WeatherApiServices(httpClient: http.Client())))),
+
+        //
+        ChangeNotifierProvider<TempSettingsProvider>(create: (context) {
+          return TempSettingsProvider();
+        }),
+
+        //
+        ChangeNotifierProxyProvider<WeatherProvider,ThemeProvider>(create:(context){
+          return ThemeProvider();
+        }, update: (BuildContext context, WeatherProvider wp, ThemeProvider? tp){
+          tp!.update(wp);
+          return tp;
+        })
       ],
 
       //
-      child: MaterialApp(
+      builder: (context,_) =>  MaterialApp(
         title: "Wheather app",
         debugShowCheckedModeBanner: false,
-        theme: ThemeData(primarySwatch: Colors.blue),
+        theme: context.watch<ThemeProvider>().state.appTheme == AppTheme.dark ? ThemeData.dark() : ThemeData.light(),
         home: Homepage(),
       ),
     );
