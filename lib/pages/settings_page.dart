6136576c0c6f3073056399519099bc6bfd8c6306
Khalid-AC <khalid.aici.ac@gmail.com>
2022-11-20 20:59:24 +0100

    settings page finished

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,22 +1,29 @@
-
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:weather_app/providers/temp_settings_provider.dart';
 
-class Settingspage extends StatefulWidget {
-  Settingspage({Key? key}) : super(key: key);
+class Settingspage extends StatelessWidget {
+  const Settingspage({Key? key}) : super(key: key);
 
-  @override
-  State<Settingspage> createState() => _SettingspageState();
-}
-
-class _SettingspageState extends State<Settingspage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Weather"),
+      appBar: AppBar(
+        title: const Text("Settings"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+        child: ListTile(
+          title: const Text("Temperature Unit"),
+          subtitle: context.watch<TempSettingsProvider>().state.tempUnit == TempUnit.celsius ? const Text("Celsius") : const Text("Fahrenheit"),
+          trailing: Switch(
+              value: context.watch<TempSettingsProvider>().state.tempUnit ==
+                  TempUnit.celsius,
+              onChanged: (_) {
+                context.read<TempSettingsProvider>().toggleTempUnit();
+              }),
         ),
-        body: Center(
-          child: Text("Settings"),
-        ));
+      ),
+    );
   }
 }
