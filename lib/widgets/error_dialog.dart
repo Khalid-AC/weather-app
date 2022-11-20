67b8e14c56cc838221cb7ef35d4a6c2f13f22d59
Khalid-AC <khalid.aici.ac@gmail.com>
2022-11-20 16:11:20 +0100

    error dialog added

---
+++ lib/widgets/error_dialog.dart
@@ -0,0 +1,40 @@
+import "dart:io";
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+void errorDialog(BuildContext context, String errorMessage) {
+  if (Platform.isIOS) {
+    showCupertinoDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) {
+          return CupertinoAlertDialog(
+            title: const Text("Error"),
+            content: Text(errorMessage),
+            actions: [
+              CupertinoDialogAction(
+                child: const Text("OK"),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              )
+            ],
+          );
+        });
+  }
+  if (Platform.isAndroid) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text("Error"),
+            content: Text(errorMessage),
+            actions: [
+              TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text("OK"))
+            ],
+          );
+        });
+  }
+}
